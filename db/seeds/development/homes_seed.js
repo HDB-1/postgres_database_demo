643999c7be9a14f6e820e811869a1ad2a8f21363
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-13 09:20:02 +0000

    remove 'material' from homes seed so it can run and be read in db postgres

--- db/seeds/development/homes_seed.js
+++ db/seeds/development/homes_seed.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('homes').insert([
-        {id: 1, size: 3000, material: 'brick', colour: 'blue'},
-        {id: 2, size: 4000, material: 'wood', colour: 'red'},
-        {id: 3, size: 2000, material: 'stone', colour: 'green'},
-        {id: 4, size: 7000, material: 'straw', colour: 'yellow'}
+        {id: 1, size: 3000, colour: 'blue'},
+        {id: 2, size: 4000, colour: 'red'},
+        {id: 3, size: 2000, colour: 'green'},
+        {id: 4, size: 7000, colour: 'yellow'}
       ]);
     });
 };
