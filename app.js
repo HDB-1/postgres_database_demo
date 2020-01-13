d43ef4d7f75ab7c560e80de8a1afcb04d9370773
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-13 09:28:02 +0000

    demo of how to knex connect differently drawing info from the knexfile.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 7070
+const setupFile = require('./knexfile')
 
 var knex = require('knex')({
-    client: 'pg',
+    client: setupFile.development.client,       //setup connection differently
     connection: {
       host: 'localhost',
       user: 'postgres',
